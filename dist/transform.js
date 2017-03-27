79f4d8d3136af16f3078ba74a17ad833de34fb6d
Long Ho <longho@yahoo-inc.com>
2017-03-26 22:26:29 -0400

    modify .gitignore

--- dist/transform.js
+++ dist/transform.js
@@ -40,67 +40,47 @@ function extractMessageDescriptor(node, idPrefix) {
     });
     return msg;
 }
-/**
- * Create simple obj { k: v }
- *
- * @param {string} k
- * @param {string} v
- * @returns {ts.PropertyAssignment}
- */
-function createObjKeyValue(k, v) {
-    const obj = ts.createNode(ts.SyntaxKind.PropertyAssignment);
-    const key = ts.createNode(ts.SyntaxKind.Identifier);
-    key.text = k;
-    const value = ts.createNode(ts.SyntaxKind.StringLiteral);
-    value.text = v;
-    obj.name = key;
-    obj.initializer = value;
-    return obj;
-}
-/**
- * Transform MessageDescriptor to ObjectLiteralExpression
- *
- * @param {MessageDescriptor} msg
- * @param {string} [idPrefix]
- * @returns {ts.ObjectLiteralExpression}
- */
-function createIntlMessage(msg) {
-    const node = ts.createNode(ts.SyntaxKind.ObjectLiteralExpression);
-    const properties = [
-        createObjKeyValue('id', msg.id),
-        createObjKeyValue('description', msg.description),
-        createObjKeyValue('defaultMessage', msg.defaultMessage)
-    ];
-    node.properties = properties;
-    return node;
+function messagesVisitor(ctx, trans, opts) {
+    const visitor = (node) => {
+        if (node.kind !== ts.SyntaxKind.PropertyAssignment) {
+            return ts.visitEachChild(node, visitor, ctx);
+        }
+        if (node.initializer.kind !== ts.SyntaxKind.ObjectLiteralExpression) {
+            return ts.visitEachChild(node, visitor, ctx);
+        }
+        const msg = extractMessageDescriptor(node.initializer, opts.idPrefix);
+        trans[node.name.getText()] = msg;
+        const newMsgNode = ts.createNode(ts.SyntaxKind.ObjectLiteralExpression);
+        // Convert translations to raw json object
+        newMsgNode.properties = Object.keys(msg).map(k => {
+            const obj = ts.createNode(ts.SyntaxKind.PropertyAssignment);
+            const key = ts.createNode(ts.SyntaxKind.Identifier);
+            const value = ts.createNode(ts.SyntaxKind.StringLiteral);
+            key.text = k;
+            value.text = msg[k];
+            obj.name = key;
+            obj.initializer = value;
+            return obj;
+        });
+        if (typeof opts.onMsgExtracted === 'function') {
+            opts.onMsgExtracted(trans);
+        }
+        const newAssignment = ts.createNode(ts.SyntaxKind.PropertyAssignment);
+        newAssignment.name = node.name;
+        newAssignment.initializer = newMsgNode;
+        return newAssignment;
+    };
+    return visitor;
 }
 function default_1(opts) {
     return (ctx) => {
         const visitor = (node) => {
             switch (node.kind) {
                 case ts.SyntaxKind.CallExpression:
+                    console.log(node.expression.getText());
                     if (node.expression.getText() === exports.DEFINE_MESSAGES_HOOK) {
                         const trans = {};
-                        ts.forEachChild(node.arguments[0], (node) => {
-                            if (node.kind !== ts.SyntaxKind.PropertyAssignment) {
-                                return;
-                            }
-                            trans[node.name.getText()] = extractMessageDescriptor(node.initializer, opts.idPrefix);
-                        });
-                        const newNode = ts.createNode(ts.SyntaxKind.ObjectLiteralExpression);
-                        // Convert translations to raw json object
-                        newNode.properties = Object.keys(trans).map(k => {
-                            const obj = ts.createNode(ts.SyntaxKind.PropertyAssignment);
-                            const key = ts.createNode(ts.SyntaxKind.Identifier);
-                            key.text = k;
-                            obj.name = key;
-                            obj.initializer = createIntlMessage(trans[k]);
-                            return obj;
-                        });
-                        if (typeof opts.onMsgExtracted === 'function') {
-                            opts.onMsgExtracted(trans);
-                        }
-                        return newNode;
+                        return ts.visitEachChild(node, messagesVisitor(ctx, trans, opts), ctx);
                     }
                 // Fallthru
                 default:
